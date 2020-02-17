5efe0d247d718209a1f071bc90d9fb1103e41150
André Luís Menegatti <andre.menegatti@hotmail.com>
2020-02-17 11:33:28 -0300

    Incluindo dados de dez/2019

--- tabelas_boletim_municipios.R
+++ tabelas_boletim_municipios.R
@@ -1,25 +1,31 @@
 library(cagedExplorer)
 
 # Carregando dados (atencao para inicio e fim)
-df_acum_ano_atual <- read_caged_rds(data_folder = 'data', start = '2018-12', end = '2019-11')
-df_acum_ano_anterior <- read_caged_rds(data_folder = 'data', start = '2017-12', end = '2018-11')
+df_acum_ano_atual <- read_caged_rds(data_folder = 'data',
+                                    start = '2019-01', end = '2019-12',
+                                    periodos_12meses = FALSE)
+
+df_acum_ano_anterior <- read_caged_rds(data_folder = 'data',
+                                       start = '2018-01', end = '2018-12',
+                                       periodos_12meses = FALSE)
 
 # Mes de referencia
-mes_atual <- 11
+mes_atual <- 12
 
 # Vetor com nomes das colunas das tabelas finais
 colunas <- c('Setores',
-             'Nov./2018',
-             'Acumulado Dez./2017 a Nov./2018',
-             'Nov./2019',
-             'Acumulado Dez./2018 a Nov./2019')
+             'Dez./2018',
+             'Acumulado Jan./2018 a Dez./2018',
+             'Dez./2019',
+             'Acumulado Jan./2019 a Dez./2019')
 
 # Pasta onde as tabelas serao salvas
 output_folder <- 'dados_boletim/tabelas'
 
 #### MUNICIPIOS ####
-for (mun in c('RIBEIRAO PRETO', 'SERTAOZINHO', 'CAMPINAS', 'FRANCA', 'SAO JOSE DO RIO PRETO')) {
-
+for (mun in c('RIBEIRAO PRETO', 'SERTAOZINHO',
+              'CAMPINAS', 'FRANCA', 'SAO JOSE DO RIO PRETO')) {
+  
   criar_tabela_municipio(df_ano_atual = df_acum_ano_atual,
                          df_ano_anterior = df_acum_ano_anterior,
                          mes_atual = mes_atual,
@@ -32,7 +38,8 @@ for (mun in c('RIBEIRAO PRETO', 'SERTAOZINHO', 'CAMPINAS', 'FRANCA', 'SAO JOSE D
 criar_tabela_regiao_adm(df_ano_atual = df_acum_ano_atual,
                         df_ano_anterior = df_acum_ano_anterior,
                         mes_atual = mes_atual, colunas = colunas,
-                        csv_output = str_c(output_folder, '/REGIAO_ADM_RIBEIRAO.csv'))
+                        csv_output = str_c(output_folder,
+                                           '/REGIAO_ADM_RIBEIRAO.csv'))
 
 #### ESTADO SP ####
 criar_tabela_estado(df_ano_atual = df_acum_ano_atual,
